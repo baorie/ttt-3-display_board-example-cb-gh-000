d775e0bcfa0ed1ad899f417ea108a2168a4453fd
Rielle Quiambao <rmq6@duke.edu>
2018-07-19 01:29:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def cell
-  
+def rows
+  puts "   |   |   "
 end
+
+def line
+  puts "-----------"
+end
+
+rows
+line
+rows
+line
+rows
